7c0c9e22d973ce8a0582d0eeebc7c9e4e433246c
migueldewoutersufv <miguel.dewouters@gmail.com>
2022-04-19 18:33:37 +0200

    mis reservas

--- src/main/java/com/andres/Proyecto_Fin_de_Grado/Controller/ReservasController.java
+++ src/main/java/com/andres/Proyecto_Fin_de_Grado/Controller/ReservasController.java
@@ -2,11 +2,15 @@ package com.andres.Proyecto_Fin_de_Grado.Controller;
 
 import com.andres.Proyecto_Fin_de_Grado.Model.Muelle;
 import com.andres.Proyecto_Fin_de_Grado.Model.Reserva;
+import com.andres.Proyecto_Fin_de_Grado.Model.Usuario;
 import com.andres.Proyecto_Fin_de_Grado.Repository.RepositorioMuelle;
 import com.andres.Proyecto_Fin_de_Grado.Repository.RepositorioReserva;
 import com.andres.Proyecto_Fin_de_Grado.Repository.RepositorioUsuario;
 import com.andres.Proyecto_Fin_de_Grado.Service.ServicioMuelle;
 import com.andres.Proyecto_Fin_de_Grado.Service.ServicioReserva;
+import com.andres.Proyecto_Fin_de_Grado.Service.ServicioUsuario;
+import com.andres.Proyecto_Fin_de_Grado.utilidades.DecodificarJWT;
+import com.andres.Proyecto_Fin_de_Grado.utilidades.JWT;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,6 +27,7 @@ public class ReservasController {
     private final RepositorioReserva repositorioReserva;
     private final RepositorioMuelle repositorioMuelle;
     private final RepositorioUsuario repositorioUsuario;
+    private final ServicioUsuario servicioUsuarioImp;
 
     //ADMIN
     @GetMapping("/reservas")
@@ -52,6 +57,15 @@ public class ReservasController {
     @GetMapping("/reserva/pantalla")
     public Collection<Reserva> reservasPantalla( ){
         return servicioReserva.reservasPantalla();
+    }
+
+
+    @GetMapping("/mis_reservas")
+    public Collection<Reserva>  misReservas(@RequestHeader Map<String, String> headers ) {
+        JWT token = DecodificarJWT.decode(headers.get("authorization"));
+
+        Usuario usu = servicioUsuarioImp.getUsuarioPorNombreUsuario(token.getNombreUsuario());
+        return usu.getReservas();
     }
 
 }
